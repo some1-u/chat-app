da4c19cf11125d71f8532495759c6c7571d59c91
saman <samanyu.maha@gmail.com>
2026-07-20 07:15:03 -0700

    Added vite config file to enable control over the dist folder for hte auth.html file.

---
+++ Frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'auth.html'),
+      },
+    },
+  },
+})
